9b530b25b7e1264dac70b2621907fe6456cd2ab4
BhargavBhagyabatRahang <145565539+BhargavBhagyabatRahang@users.noreply.github.com>
2023-11-06 09:04:21 +0530

    Update Password_Manager.java
    Add button command has been enabled

--- Password_Manager.java
+++ Password_Manager.java
@@ -1,11 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 import javax.swing.border.TitledBorder;
 
-class GUI{
+class GUI_2{
 
     public static void main(String[]args){
         JPanel panel = new JPanel();
@@ -16,6 +17,22 @@ class GUI{
         frame.add(panel);
 
         panel.setLayout(null);
+        
+        //an array of strings to add and remove from the list whenever a button is called
+        String[] header = {"Website", "Username", "Password"};
+        String[][] rec = {{"Website", "Username", "Password"},{null,null,null}};
+
+        //table to store array elements in
+        JTable table = new JTable(rec, header);
+        table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        table.setShowHorizontalLines(true);
+        table.setShowGrid(true);
+        table.setGridColor(Color.gray);
+        table.setBounds(10, 50,680, 400);
+        panel.add(new JScrollPane(table));
+        panel.add(table);
+
+        
         //create a new username, password and website labels
         JLabel userlabel = new JLabel("Username");
         JLabel passlabel = new JLabel("Password");
@@ -49,28 +66,72 @@ class GUI{
         JButton addbutton = new JButton("Add");
         addbutton.setBounds(300, 630, 80, 25);
         panel.add(addbutton);
-
-
-
+        
+        //Adding an actionlistener to invoke add button 
+        
+        ActionListener actionListener=new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) // Function to invoke add button
+            {
+                 String username=usertext.getText();                // Extracting username from the text field
+                 String password=passtext.getText();                // Extracting password from the text field
+                 String webaddress=webtext.getText();               // Extracting webaddress from the text field 
+                 
+                 if (username.equals("") || password.equals("")||webaddress.equals(""))
+                 {
+                     System.out.println("Empty parameter");
+                 }
+                 else
+                 {
+                     
+                 String[] newrec={webaddress,username,password};    // Creating an array to append the new record into the table
+                 
+                 String[][] temp=new String[rec.length][3];         // Creating an temporary array to backup the existing records
+                 
+                 int x=rec.length;                                  // Storing the value of no. of rows
+                 for (int i=0;i<temp.length;i++)
+                 {
+                     temp[i]=rec[i];
+                 }
+                 
+                 String[] header = {"Website", "Username", "Password"};
+                 String[][] rec=new String[x+1][3];                            // Redefining the 'rec' array
+                 
+                 for (int i=0;i<temp.length;i++)
+                 {
+                     rec[i]=temp[i];                                           // pasting the previous record
+                 }
+                 
+                 rec[rec.length-1]=newrec;                                     // Appening the new record
+                 
+                 panel.remove(table);
+                 
+                 //table to store array elements in
+                 
+                 JTable table = new JTable(rec, header);
+                 table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+                 table.setShowHorizontalLines(true);
+                 table.setShowGrid(true);
+                 table.setGridColor(Color.gray);
+                 table.setBounds(10, 50,680, 400);
+                 panel.add(new JScrollPane(table));
+                 panel.add(table);
+                 
+                 panel.revalidate();
+                 panel.repaint();
+                 
+                 System.out.println("Hello");
+                   
+                 }
+                 
+            }
+        };
+        
+        addbutton.addActionListener(actionListener);
+            
         JButton removebutton = new JButton("Remove");
         removebutton.setBounds(400, 630, 80 , 25);
         panel.add(removebutton);
-
-        //an array of strings to add and remove from the list whenever a button is called
-        String[] header = {"Website", "Username", "Password"};
-        String[][] rec = {{"www.youtube.com", "revanth", "password1234"}
-        , {"www.yahoo.com", "john@yahoo.com", "123456"},
-                {"www.google.com", "hello", "test123"}};
-
-        //table to store array elements in
-        JTable table = new JTable(rec, header);
-        table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-        table.setShowHorizontalLines(true);
-        table.setShowGrid(true);
-        table.setGridColor(Color.gray);
-        table.setBounds(10, 50,680, 400);
-        panel.add(new JScrollPane(table));
-        panel.add(table);
 
         frame.setVisible(true);
     }
