ed975b4fbc7b36e8ee1e91ddcdce463ead620f17
r-kolla <145572936+r-kolla@users.noreply.github.com>
2023-11-07 18:22:32 +0530

    Update Password_Manager.java
    added "add button" and some parts of remove button

--- Password_Manager.java
+++ Password_Manager.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
 
 class GUI_2{
 
@@ -17,13 +18,14 @@ class GUI_2{
         frame.add(panel);
 
         panel.setLayout(null);
-        
+
         //an array of strings to add and remove from the list whenever a button is called
         String[] header = {"Website", "Username", "Password"};
-        String[][] rec = {{"Website", "Username", "Password"},{null,null,null}};
+        String[][] rec = {{"", "", ""},{"","",""}};
 
         //table to store array elements in
-        JTable table = new JTable(rec, header);
+        JTable table = new JTable(0,3);
+
         table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
         table.setShowHorizontalLines(true);
         table.setShowGrid(true);
@@ -32,7 +34,7 @@ class GUI_2{
         panel.add(new JScrollPane(table));
         panel.add(table);
 
-        
+
         //create a new username, password and website labels
         JLabel userlabel = new JLabel("Username");
         JLabel passlabel = new JLabel("Password");
@@ -54,7 +56,7 @@ class GUI_2{
         panel.add(usertext);
 
 
-        JPasswordField passtext = new JPasswordField();
+        JTextField passtext = new JTextField();
         passtext.setBounds(100, 630, 165, 25);
         panel.add(passtext);
 
@@ -66,72 +68,56 @@ class GUI_2{
         JButton addbutton = new JButton("Add");
         addbutton.setBounds(300, 630, 80, 25);
         panel.add(addbutton);
-        
-        //Adding an actionlistener to invoke add button 
-        
+
+        //Adding an actionlistener to invoke add button
+
         ActionListener actionListener=new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) // Function to invoke add button
             {
-                 String username=usertext.getText();                // Extracting username from the text field
-                 String password=passtext.getText();                // Extracting password from the text field
-                 String webaddress=webtext.getText();               // Extracting webaddress from the text field 
-                 
-                 if (username.equals("") || password.equals("")||webaddress.equals(""))
-                 {
-                     System.out.println("Empty parameter");
-                 }
-                 else
-                 {
-                     
-                 String[] newrec={webaddress,username,password};    // Creating an array to append the new record into the table
-                 
-                 String[][] temp=new String[rec.length][3];         // Creating an temporary array to backup the existing records
-                 
-                 int x=rec.length;                                  // Storing the value of no. of rows
-                 for (int i=0;i<temp.length;i++)
-                 {
-                     temp[i]=rec[i];
-                 }
-                 
-                 String[] header = {"Website", "Username", "Password"};
-                 String[][] rec=new String[x+1][3];                            // Redefining the 'rec' array
-                 
-                 for (int i=0;i<temp.length;i++)
-                 {
-                     rec[i]=temp[i];                                           // pasting the previous record
-                 }
-                 
-                 rec[rec.length-1]=newrec;                                     // Appening the new record
-                 
-                 panel.remove(table);
-                 
-                 //table to store array elements in
-                 
-                 JTable table = new JTable(rec, header);
-                 table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-                 table.setShowHorizontalLines(true);
-                 table.setShowGrid(true);
-                 table.setGridColor(Color.gray);
-                 table.setBounds(10, 50,680, 400);
-                 panel.add(new JScrollPane(table));
-                 panel.add(table);
-                 
-                 panel.revalidate();
-                 panel.repaint();
-                 
-                 System.out.println("Hello");
-                   
-                 }
-                 
+
+                String data[] = {usertext.getText(), passtext.getText(), webtext.getText()};
+                DefaultTableModel tblmodel = (DefaultTableModel)table.getModel();
+
+                if (usertext.equals("") || passtext.equals("")||webtext.equals(""))
+                {
+                    System.out.println("Empty parameter");
+                    JOptionPane.showMessageDialog(null, "Enter all fields");
+                }
+                else
+                {
+
+                    tblmodel.addRow(data);
+
+                    JOptionPane.showMessageDialog(null, "Successfully added!");
+
+                    usertext.setText("");
+                    passtext.setText("");
+                    webtext.setText("");
+
+                }
+
             }
         };
-        
+
         addbutton.addActionListener(actionListener);
-            
+
         JButton removebutton = new JButton("Remove");
         removebutton.setBounds(400, 630, 80 , 25);
         panel.add(removebutton);
+
+        ActionListener actionListener1=new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent ae){
+                table.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+
+                if(table.getSelectedRow() != -1) {
+
+                    //tblmodel.removeRow(table.getSelectedRow());
+                    JOptionPane.showMessageDialog(null, "Selected row deleted successfully");
+                }
+            }
+        };
 
         frame.setVisible(true);
     }
